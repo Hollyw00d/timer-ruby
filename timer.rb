268f6ac15c14e9139050e476cfe14f8f6bb0b9ba
Hollyw00d <matt@mattjennings.net>
2015-08-06 18:47:19 -0700

    middle of assgnmnt

---
+++ timer.rb
@@ -0,0 +1,17 @@
+def timer
+  start_time = Time.new
+  yield
+  end_time = Time.new
+  end_time - start_time
+end
+
+# "sleep" suspends the current thread
+# for a number of seconds so "sleep(0.5)"
+# waits 0.5 seconds when "yield" is called in
+# the method above and then continues with the
+# execution for the "timer" method including:
+# "end_time = Time.new"
+# "end_time - start_time" (this code returns for the final result)
+total_time = timer{ sleep(0.5) }
+
+puts "Above 0.5 seconds" if total_time > 0.5
