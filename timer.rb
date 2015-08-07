6e883f45f57034970430b2f77ab0fd94e022a616
Hollyw00d <matt@mattjennings.net>
2015-08-07 10:13:26 -0700

    assgnmnt done

--- timer.rb
+++ timer.rb
@@ -6,12 +6,12 @@ def timer
 end
 
 # "sleep" suspends the current thread
-# for a number of seconds so "sleep(0.5)"
-# waits 0.5 seconds when "yield" is called in
+# for a number of seconds so "sleep(1)"
+# waits 1 seconds when "yield" is called in
 # the method above and then continues with the
 # execution for the "timer" method including:
 # "end_time = Time.new"
 # "end_time - start_time" (this code returns for the final result)
-total_time = timer{ sleep(0.5) }
+total_time = timer{ sleep(1) }
 
 puts "Above 0.5 seconds" if total_time > 0.5
--- timer_spec.rb
+++ timer_spec.rb
@@ -2,7 +2,11 @@ require_relative "timer"
 
 RSpec.describe "My timer method" do
   it "should time how long the block takes" do
-    total_time = timer { sleep(0.5) }
+    # "sleep" suspends the current thread
+    # for a number of seconds so "sleep(1)"
+    # waits 1 seconds when "yield" is called in
+    # the "timer" method
+    total_time = timer { sleep(1) }
     expect(total_time > 0.509).to eq(true)
     expect(total_time > 0.5).to eq(true)
   end
